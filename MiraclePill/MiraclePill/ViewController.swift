82bc8ee1ce30b700bed785d4c9276c31209650be
Nikolas Ponomarov <beardings@users.noreply.github.com>
2017-08-22 01:47:00 +0300

    add some image add start develop this app

---
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Nikolas Ponomarov on 22.08.17.
+//  Copyright © 2017 Nikolas Ponomarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
